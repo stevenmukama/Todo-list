e3326486ebc89551c3a720c4ba37bdaa9162b760
Mukama Steven <stevenmukama1@gmail.com>
2022-12-09 10:07:10 +0200

    ADDtod

---
+++ src/tasks.js
@@ -0,0 +1,16 @@
+import 'lodash';
+import './style.css';
+import todoList from './modules/index.js';
+
+const todoTask = document.querySelector('.tasks-list');
+
+todoList.forEach((item) => {
+  todoTask.innerHTML += ` <div class = "todo-list" id = ${item.index} complete = ${item.completed}>
+      <div class = "checkbox-description-container">
+           <input type='checkbox'>
+          <p>${item.description}</p>
+      </div> 
+        <i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+       <div class="line"></div>`;
+});
